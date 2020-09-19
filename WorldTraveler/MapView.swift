09c576dc4f9f033451217d27b6b31eb89b711852
hanifek <hanifekurnaz@outlook.com>
2020-09-19 00:31:42 -0700

    first project

---
+++ WorldTraveler/MapView.swift
@@ -0,0 +1,52 @@
+//
+//  MapView.swift
+//  WorldTraveler
+//
+//  Created by hks on 18.09.2020.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    static var regions: [MKCoordinateRegion] = [
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.0, longitude: 36.0),
+            span: MKCoordinateSpan(latitudeDelta: 4.0, longitudeDelta: 4.0)
+            ),
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.0, longitude: -0.11),
+            span: MKCoordinateSpan(latitudeDelta: 4.0, longitudeDelta: 4.0)
+            )
+        ]
+    
+    @State var region : MKCoordinateRegion = regions[0]
+    @State var selectedIndex = 0
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.top)
+            .overlay(
+                VStack
+                {
+                    Picker("Picker", selection: $selectedIndex, content: {
+                            Text("MAP_CITY_SAMSUN").tag(0)
+                            Text("MAP_CITY_LONDRA").tag(1)
+                        }
+                    ).pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    .onChange(of: selectedIndex, perform: { value in
+                        self.region = MapView.regions[selectedIndex]
+                    })
+                    Spacer()
+                }
+            )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
